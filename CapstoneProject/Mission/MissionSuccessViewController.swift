cbee9f48412a899a7b93a076e7b81024691611b5
yujin <yujin3289@naver.com>
2024-05-28 23:04:33 +0900

    storyboardchange

---
+++ CapstoneProject/Mission/MissionSuccessViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MissionSuccessViewController.swift
+//  CapstoneProject
+//
+//  Created by yujin on 5/28/24.
+//
+
+import UIKit
+
+class MissionSuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
